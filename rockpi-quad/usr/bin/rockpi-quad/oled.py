47c1679c902e42b0331c08d31e3e97e93eab4924
nyok92 <40342954+nyok92@users.noreply.github.com>
2025-01-13 18:18:11 +0100

    Update oled.py

--- rockpi-quad/usr/bin/rockpi-quad/oled.py
+++ rockpi-quad/usr/bin/rockpi-quad/oled.py
@@ -12,6 +12,7 @@ from PIL import ImageFont
 import multiprocessing as mp
 
 import misc
+import fan
 
 font = {
     '10': ImageFont.truetype('fonts/DejaVuSansMono-Bold.ttf', 10),
@@ -68,8 +69,22 @@ def put_disk_info():
             {'xy': (0, 10), 'text': text2, 'fill': 255, 'font': font['11']},
             {'xy': (0, 21), 'text': text3, 'fill': 255, 'font': font['11']},
         ]
+    elif len(k) == 4:
+        text2 = '{} {}  {} {}'.format(k[1], v[1], k[2], v[2])
+        text3 = '{} {}'.format(k[3], v[3])
+        page = [
+            {'xy': (0, -2), 'text': text1, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': text2, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 21), 'text': text3, 'fill': 255, 'font': font['11']},
+        ]
     elif len(k) == 3:
         text2 = '{} {}  {} {}'.format(k[1], v[1], k[2], v[2])
+        page = [
+            {'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['12']},
+            {'xy': (0, 18), 'text': text2, 'fill': 255, 'font': font['12']},
+        ]
+    elif len(k) == 2:
+        text2 = '{} {}'.format(k[1], v[1])
         page = [
             {'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['12']},
             {'xy': (0, 18), 'text': text2, 'fill': 255, 'font': font['12']},
@@ -78,6 +93,81 @@ def put_disk_info():
         page = [{'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['14']}]
 
     return page
+
+def put_disk_temp_info(pages_len):
+    page = {}
+    k, v = misc.get_disk_temp_info()
+
+    if len(k) == 0:
+        return page
+
+    text1 = 'Disks Temp:'
+
+    if len(k) == 4:
+        text2 = '{} {}  {} {}'.format(k[0], v[0], k[1], v[1])
+        text3 = '{} {}  {} {}'.format(k[2], v[2], k[3], v[3])
+        page[pages_len] = [
+            {'xy': (0, -2), 'text': text1, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': text2, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 21), 'text': text3, 'fill': 255, 'font': font['11']},
+        ]
+    elif len(k) == 3:
+        text2 = '{} {}  {} {}'.format(k[0], v[0], k[1], v[1])
+        text3 = '{} {}'.format(k[2], v[2])
+        page[pages_len] = [
+            {'xy': (0, -2), 'text': text1, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': text2, 'fill': 255, 'font': font['11']},
+            {'xy': (0, 21), 'text': text3, 'fill': 255, 'font': font['11']},
+        ]
+    elif len(k) == 2:
+        text2 = '{} {}  {} {}'.format(k[0], v[0], k[1], v[1])
+        page[pages_len] = [
+            {'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['12']},
+            {'xy': (0, 18), 'text': text2, 'fill': 255, 'font': font['12']},
+        ]
+    elif len(k) == 1:
+        text2 = '{}'.format(k[0], v[0])
+        page[pages_len] = [
+            {'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['12']},
+            {'xy': (0, 18), 'text': text2, 'fill': 255, 'font': font['12']},
+        ]
+    else:
+        page[pages_len] = [{'xy': (0, 2), 'text': text1, 'fill': 255, 'font': font['14']}]
+
+    return page
+    
+def put_disk_io_info(pages_len):
+    pages = {}
+    page_index = pages_len
+    disks = misc.get_disk_list('io_usage_mnt_points')
+
+    for x in disks:
+        x = misc.delete_disk_partition_number(x)
+
+        pages[page_index] = [
+            {'xy': (0, -2), 'text': 'Disk (' + x + '):', 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': misc.get_disk_io_read_info(x), 'fill': 255, 'font': font['11']},
+            {'xy': (0, 21), 'text': misc.get_disk_io_write_info(x), 'fill': 255, 'font': font['11']}
+        ]
+        page_index = page_index + 1
+
+    return pages
+
+
+def put_interface_info(pages_len):
+    pages = {}
+    page_index = pages_len
+    interfaces = misc.get_interface_list()
+
+    for x in interfaces:
+        pages[page_index] = [
+            {'xy': (0, -2), 'text': 'Network (' + x + '):', 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': misc.get_interface_rx_info(x), 'fill': 255, 'font': font['11']},
+            {'xy': (0, 21), 'text': misc.get_interface_tx_info(x), 'fill': 255, 'font': font['11']}
+        ]
+        page_index = page_index + 1
+
+    return pages
 
 
 def gen_pages():
@@ -88,11 +178,16 @@ def gen_pages():
             {'xy': (0, 21), 'text': misc.get_info('ip'), 'fill': 255, 'font': font['11']},
         ],
         1: [
-            {'xy': (0, 2), 'text': misc.get_info('cpu'), 'fill': 255, 'font': font['12']},
-            {'xy': (0, 18), 'text': misc.get_info('men'), 'fill': 255, 'font': font['12']},
+            {'xy': (0, -2), 'text': 'Fan speed: ' + str(fan.get_dc()) + '%', 'fill': 255, 'font': font['11']},
+            {'xy': (0, 10), 'text': misc.get_info('cpu'), 'fill': 255, 'font': font['12']},
+            {'xy': (0, 21), 'text': misc.get_info('men'), 'fill': 255, 'font': font['12']}
         ],
         2: put_disk_info()
     }
+
+    pages.update(put_interface_info(len(pages)))
+    pages.update(put_disk_temp_info(len(pages)))
+    pages.update(put_disk_io_info(len(pages)))
 
     return pages
 
